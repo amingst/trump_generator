1e489405ad6b05af3cb8d0f776b04dd32af69cd7
Andrew Mingst <amingst97@gmail.com>
2019-10-23 18:57:18 -0400

    created dict mappings for tweets

--- markov.py
+++ markov.py
@@ -1,4 +1,20 @@
 import numpy as np
 
 tweets = open('trump_tweet.txt', encoding='utf8').read()
-tweet_map = tweets.split()
+tweet_text = tweets.split()
+
+
+def gen_pairs(text):
+    for i in range(len(text) - 1):
+        yield (text[i], text[i + 1])
+
+
+tweet_map = gen_pairs(tweet_text)
+
+tweet_dict = {}
+
+for a, b in tweet_map:
+    if a in tweet_dict.keys():
+        tweet_dict[a].append(b)
+    else:
+        tweet_dict[a] = [b]
