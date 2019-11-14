2a29e6c7192935ceac91d77026b10b3d56f8f1d5
Andrew Mingst <amingst97@gmail.com>
2019-11-14 18:28:21 -0500

    created api to generate tweets, removed other js

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+app.listen(3000, console.log("running"));
+
+app.get("/tweet", generateTweet);
+
+function generateTweet(req, res) {
+  let spawn = require("child_process").spawn;
+
+  let process = spawn("python", ["./markov_generator.py"]);
+
+  process.stdout.on("data", function(data) {
+    res.send(data.toString());
+  });
+}
--- js/FileUtils.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-
-module.exports.writeTweet = (filepath, tweet) => {
-    fs.writeFileSync(filepath, tweet, (err) => {
-        if (err) throw err
-      })
-}
